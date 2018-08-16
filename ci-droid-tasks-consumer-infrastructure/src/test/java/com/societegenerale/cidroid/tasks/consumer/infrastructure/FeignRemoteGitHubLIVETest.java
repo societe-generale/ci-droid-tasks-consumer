ebfdca1f9fb92f06b3f42fd6381b615e79939d9b
vfuchs070114 <vincent.fuchs@sgcib.com>
2018-08-16 10:54:28 +0530

    now using github OAuth token for bulk actions

--- ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/FeignRemoteGitHubLIVETest.java
+++ ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/FeignRemoteGitHubLIVETest.java
@@ -1,8 +1,12 @@
 package com.societegenerale.cidroid.tasks.consumer.infrastructure;
 
 import com.societegenerale.cidroid.tasks.consumer.infrastructure.config.InfraConfig;
+import com.societegenerale.cidroid.tasks.consumer.services.GitHubContentBase64codec;
+import com.societegenerale.cidroid.tasks.consumer.services.exceptions.GitHubAuthorizationException;
 import com.societegenerale.cidroid.tasks.consumer.services.model.github.Comment;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.DirectCommit;
 import com.societegenerale.cidroid.tasks.consumer.services.model.github.ResourceContent;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.UpdatedResource;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes={ InfraConfig.class,LiveTestConfig.class}, initializers = YamlFileApplicationContextInitializer.class)
 @TestPropertySource("/application-test.yml")
-@Ignore("to launch manually and test in local")
+@Ignore("to launch manually and test in local - you probably need to update config before running")
 public class FeignRemoteGitHubLIVETest {
 
     @Autowired
@@ -25,21 +29,33 @@ public class FeignRemoteGitHubLIVETest {
     @Test
     public void manualTestForPublishingComment(){
 
-        // you need to update properties with gitHub.url="https://sgithub.fr.world.socgen"
-
-        feignRemoteGitHub.addCommentDescribingRebase("myOrga/test-vf-ci-droid", 2, new Comment("test Vincent"));
+        feignRemoteGitHub.addCommentDescribingRebase("myOrga/myProject", 2, new Comment("test Vincent"));
 
     }
 
     @Test
-
     public void manualTestForFetchingAResource() {
-
-        // you need to update properties with gitHub.url="https://sgithub.fr.world.socgen"
 
         ResourceContent resourceContent = feignRemoteGitHub.fetchContent("myOrga/myProject", "null", "master");
 
         assertThat(resourceContent).isNull();
+    }
+
+    @Test
+    public void manualTestForUpdatingAResource() throws GitHubAuthorizationException {
+
+        DirectCommit directCommit = new DirectCommit();
+        directCommit.setBranch("master");
+        directCommit.setCommitter(new DirectCommit.Committer("vincent-fuchs", "vincent.fuchs@gmail.com"));
+        directCommit.setCommitMessage("test performed on behalf of vincent-fuchs by CI-droid");
+
+        directCommit.setBase64EncodedContent(GitHubContentBase64codec.encode("hello world with OAuth token"));
+
+        UpdatedResource updatedResource = feignRemoteGitHub.updateContent("myOrga/myProject",
+                                                                          "testFile3.txt",
+                                                                          directCommit, "someToken123456");
+
+        assertThat(updatedResource).isNull();
     }
 
 }
