0827953ddc707932e3be5ceecc71f5a0d38b53db
vfuchs070114 <vincent.fuchs@sgcib.com>
2018-11-14 21:10:33 +0530

    Revert "cleaning up useless dummy classes"
    This reverts commit faa22bc

--- ci-droid-tasks-consumer-infrastructure/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/config/InfraConfig.java
+++ ci-droid-tasks-consumer-infrastructure/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/config/InfraConfig.java
@@ -7,10 +7,13 @@ import com.societegenerale.cidroid.tasks.consumer.infrastructure.FeignRemoteGitH
 import com.societegenerale.cidroid.tasks.consumer.infrastructure.GithubEventListener;
 import com.societegenerale.cidroid.tasks.consumer.infrastructure.notifiers.EMailActionNotifier;
 import com.societegenerale.cidroid.tasks.consumer.services.*;
+import com.societegenerale.cidroid.tasks.consumer.services.actionHandlers.DummyPullRequestEventHandler;
+import com.societegenerale.cidroid.tasks.consumer.services.actionHandlers.DummyPushEventOnDefaultBranchHandler;
 import com.societegenerale.cidroid.tasks.consumer.services.actionHandlers.PullRequestEventHandler;
 import com.societegenerale.cidroid.tasks.consumer.services.actionHandlers.PushEventOnDefaultBranchHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignAutoConfiguration;
@@ -101,6 +104,20 @@ public class InfraConfig {
     }
 
     @Bean
+    @ConditionalOnMissingBean(PushEventOnDefaultBranchHandler.class)
+    public PushEventOnDefaultBranchHandler dummyPushEventOnDefaultBranchHandler(){
+
+        return new DummyPushEventOnDefaultBranchHandler();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(PullRequestEventHandler.class)
+    public PullRequestEventHandler dummyPullRequestEventHandler(){
+
+        return new DummyPullRequestEventHandler();
+    }
+
+    @Bean
     public HttpMessageConverters feignHttpMessageConverters() {
         return new HttpMessageConverters();
     }
@@ -112,11 +129,13 @@ public class InfraConfig {
         return new ActionToPerformService(remoteGitHub, notificationService);
     }
 
+
     @Bean
     public ActionNotificationService actionNotificationService(MailSender javaMailSender,
             @Value("${spring.mail.sender}") String mailSender) {
 
         return new ActionNotificationService(new EMailActionNotifier(javaMailSender, mailSender));
+
     }
 
 }
