48f5283e95a5a0d395e69b233004d6b8929515dd
Juliette de Rancourt <derancourt.juliette@gmail.com>
2019-01-09 20:28:51 +0100

    Refactor & plug rebase IT on new mock server

---
+++ ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/PullRequestRebaseIT.java
@@ -0,0 +1,98 @@
+package com.societegenerale.cidroid.tasks.consumer.infrastructure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.societegenerale.cidroid.tasks.consumer.infrastructure.config.InfraConfig;
+import com.societegenerale.cidroid.tasks.consumer.infrastructure.mocks.GitHubMockServer;
+import com.societegenerale.cidroid.tasks.consumer.services.GitCommit;
+import com.societegenerale.cidroid.tasks.consumer.services.Rebaser;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.PullRequest;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.PushEvent;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.verify.VerificationTimes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.societegenerale.cidroid.tasks.consumer.infrastructure.mocks.GitHubMock.GITHUB_MOCK_PORT;
+import static com.societegenerale.cidroid.tasks.consumer.services.model.github.PRmergeableStatus.MERGEABLE;
+import static java.util.Collections.singletonList;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.when;
+import static org.mockserver.model.HttpRequest.request;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { InfraConfig.class, TestConfig.class },
+        initializers = YamlFileApplicationContextInitializer.class)
+public class PullRequestRebaseIT {
+
+    private static final int PULL_REQUEST_ID = 1347;
+    private static final String COMMIT_ID = "123456";
+    private static final String COMMIT_MESSAGE = "a tiny commit that was done on master";
+
+    @Autowired
+    private GithubEventListener githubEventListener;
+
+    @Autowired
+    private Rebaser mockRebaser;
+
+    private GitHubMockServer githubMockServer;
+    private PushEvent pushEvent;
+    private PullRequest pullRequest;
+
+    @Before
+    public void setUp() throws IOException {
+        githubMockServer = new GitHubMockServer(GITHUB_MOCK_PORT);
+        githubMockServer.start();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        pushEvent = objectMapper.readValue(
+                getClass().getClassLoader().getResourceAsStream("pushEvent.json"),
+                PushEvent.class);
+
+        pullRequest = objectMapper.readValue(
+                getClass().getClassLoader().getResourceAsStream("singlePullRequest.json"),
+                PullRequest.class);
+    }
+
+    @After
+    public void tearDown() {
+        githubMockServer.stop();
+    }
+
+    @Test
+    public void shouldRebaseMergeablePullRequest() {
+        PullRequest expectedPullRequest = argThat(pr -> pr.getNumber() == PULL_REQUEST_ID);
+        when(mockRebaser.rebase(expectedPullRequest)).thenReturn(getRebaseResult());
+
+        githubMockServer.updatePullRequestMergeabilityStatus(MERGEABLE);
+
+        githubEventListener.onGitHubPushEventOnDefaultBranch(pushEvent);
+
+        String expectedComment = "CI-droid has rebased below 1 commit(s):\\n" +
+                "- " + COMMIT_ID + " / " + COMMIT_MESSAGE + "\\n";
+
+        new MockServerClient("localhost", GITHUB_MOCK_PORT).verify(
+                request()
+                        .withMethod("POST")
+                        .withPath("/api/v3/repos/baxterthehacker/public-repo/issues/" + PULL_REQUEST_ID + "/comments")
+                        .withBody("{\"body\":\"" + expectedComment + "\"}"),
+                VerificationTimes.once()
+        );
+    }
+
+    private Pair<PullRequest, List<GitCommit>> getRebaseResult() {
+        GitCommit rebasedCommit = new GitCommit(COMMIT_ID, COMMIT_MESSAGE);
+        return new ImmutablePair<>(pullRequest, singletonList(rebasedCommit));
+    }
+
+}
--- ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/mocks/GitHubMockServer.java
+++ ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/mocks/GitHubMockServer.java
@@ -1,5 +1,8 @@
 package com.societegenerale.cidroid.tasks.consumer.infrastructure.mocks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.PRmergeableStatus;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.PullRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.mockserver.integration.ClientAndServer;
@@ -9,18 +12,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
+import static com.societegenerale.cidroid.tasks.consumer.services.model.github.PRmergeableStatus.NOT_MERGEABLE;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 @Slf4j
 public class GitHubMockServer {
 
     private ClientAndServer githubMockServer;
-
     private int port;
+    private PRmergeableStatus pullRequestMergeableStatus;
+    private ObjectMapper objectMapper;
 
     public GitHubMockServer(int port) {
         this.port = port;
+        pullRequestMergeableStatus = NOT_MERGEABLE;
+        objectMapper = new ObjectMapper();
     }
 
     public void start() {
@@ -37,6 +45,10 @@ public class GitHubMockServer {
         }
     }
 
+    public void updatePullRequestMergeabilityStatus(PRmergeableStatus status) {
+        pullRequestMergeableStatus = status;
+    }
+
     private void initRoutes() {
         githubMockServer
                 .when(request()
@@ -49,29 +61,58 @@ public class GitHubMockServer {
                 .when(request()
                         .withMethod("GET")
                         .withPath("/api/v3/repos/baxterthehacker/public-repo/pulls/[0-9]+"))
-                .respond(getPullRequest());
+                .respond(request -> getPullRequest());
 
         githubMockServer
                 .when(request()
                         .withMethod("GET")
                         .withPath("/api/v3/users/baxterthehacker"))
                 .respond(getUser());
+
+        githubMockServer
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/api/v3/repos/baxterthehacker/public-repo/issues/[0-9]+/comment"))
+                .respond(response().withStatusCode(200));
+
+        githubMockServer
+                .when(request()
+                        .withMethod("PATCH")
+                        .withPath("/api/v3/repos/baxterthehacker/public-repo/pulls/[0-9]+"))
+                .respond(response().withStatusCode(200));
     }
 
     private HttpResponse getOpenPullRequests() {
-        return HttpResponse.response()
+        return response()
                 .withBody(readFromFile("pullRequests.json"))
                 .withHeader("Content-Type", "application/json");
     }
 
     private HttpResponse getPullRequest() {
+        String pullRequestAsString = getPullRequestWithMergeabilityStatus();
+
         return HttpResponse.response()
-                .withBody(readFromFile("singlePullRequest.json"))
+                .withBody(pullRequestAsString)
                 .withHeader("Content-Type", "application/json");
     }
 
+    private String getPullRequestWithMergeabilityStatus() {
+        try {
+            PullRequest pullRequest = objectMapper.readValue(
+                    getClass().getClassLoader().getResourceAsStream("singlePullRequest.json"),
+                    PullRequest.class);
+
+            pullRequest.setMergeable(pullRequestMergeableStatus.getValue());
+
+            return objectMapper.writeValueAsString(pullRequest);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Problem reading or writing the pull request");
+        }
+    }
+
     private HttpResponse getUser() {
-        return HttpResponse.response()
+        return response()
                 .withBody(readFromFile("user.json"))
                 .withHeader("Content-Type", "application/json");
     }
