ecb1c2a90ea9f3542e0c9fbcb33611ffae0e60cb
Vincent FUCHS <vincent.fuchs@gmail.com>
2022-01-16 12:22:21 +0100

    lowering back the timeout

--- ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/handlers/PullRequestNotificationsIT.java
+++ ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/handlers/PullRequestNotificationsIT.java
@@ -34,7 +34,7 @@ public class PullRequestNotificationsIT extends SourceControlEventHandlerIT {
 
         sourceControlEventListener.onPushEventOnDefaultBranch(rawGitHubPushEvent);
 
-        await().atMost(10, SECONDS)
+        await().atMost(2, SECONDS)
                 .until(() -> assertThat(notifier.getNotifications()).hasSize(1));
 
         Pair<User, Message> notification = notifier.getNotifications().get(0);
