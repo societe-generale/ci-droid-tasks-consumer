e0b79d31d3f55e9a488f91a0e95f1f729e64bde0
Vincent Fuchs <vincent.fuchs@gefa.de>
2022-01-17 12:12:36 +0100

    replacing a custom functional interface by a standard Supplier<LocalDateTime>

--- ci-droid-tasks-consumer-services/src/test/java/com/societegenerale/cidroid/tasks/consumer/services/eventhandlers/PullRequestCleaningHandlerTest.java
+++ ci-droid-tasks-consumer-services/src/test/java/com/societegenerale/cidroid/tasks/consumer/services/eventhandlers/PullRequestCleaningHandlerTest.java
@@ -1,22 +1,5 @@
 package com.societegenerale.cidroid.tasks.consumer.services.eventhandlers;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Collections;
-
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.societegenerale.cidroid.tasks.consumer.services.RemoteSourceControl;
-import com.societegenerale.cidroid.tasks.consumer.services.model.DateProvider;
-import com.societegenerale.cidroid.tasks.consumer.services.model.PushEvent;
-import com.societegenerale.cidroid.tasks.consumer.services.model.github.GitHubPushEvent;
-import com.societegenerale.cidroid.tasks.consumer.services.model.github.PullRequest;
-import com.societegenerale.cidroid.tasks.consumer.services.monitoring.TestAppender;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
-
 import static com.societegenerale.cidroid.tasks.consumer.services.TestUtils.readFromInputStream;
 import static com.societegenerale.cidroid.tasks.consumer.services.monitoring.MonitoringEvents.OLD_PR_CLOSED;
 import static java.util.stream.Collectors.toList;
@@ -24,7 +7,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.societegenerale.cidroid.tasks.consumer.services.RemoteSourceControl;
+import com.societegenerale.cidroid.tasks.consumer.services.model.PushEvent;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.GitHubPushEvent;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.PullRequest;
+import com.societegenerale.cidroid.tasks.consumer.services.monitoring.TestAppender;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.function.Supplier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
 public class PullRequestCleaningHandlerTest {
 
@@ -33,22 +32,22 @@ public class PullRequestCleaningHandlerTest {
 
     private PullRequestCleaningHandler pullRequestCleaningHandler;
     private RemoteSourceControl remoteSourceControl;
-    private DateProvider dateProvider;
     private PushEvent pushEvent;
 
     private final TestAppender testAppender=new TestAppender();
+
+    private final Supplier<LocalDateTime> dateProvider = () -> LocalDateTime.of(2018, 12, 23, 16, 0, 0);
 
     @BeforeEach
     public void setUp() throws IOException {
         remoteSourceControl = mock(RemoteSourceControl.class);
 
-        dateProvider = () -> LocalDateTime.of(2018, 12, 23, 16, 0, 0);
 
-        pullRequestCleaningHandler = new PullRequestCleaningHandler(
-                remoteSourceControl,
-                dateProvider,
-                PR_AGE_LIMIT_IN_DAYS
-        );
+      pullRequestCleaningHandler = new PullRequestCleaningHandler(
+              remoteSourceControl,
+              dateProvider,
+              PR_AGE_LIMIT_IN_DAYS
+      );
 
         String pushEventPayload = readFromInputStream(getClass().getResourceAsStream("/pushEvent.json"));
         pushEvent = new ObjectMapper().readValue(pushEventPayload, GitHubPushEvent.class);
@@ -60,7 +59,7 @@ public class PullRequestCleaningHandlerTest {
 
     @Test
     public void shouldClosePRWhichOutlivedTheLimit() {
-        LocalDateTime oldDate = dateProvider.now().minusDays(PR_AGE_LIMIT_IN_DAYS + 10);
+        LocalDateTime oldDate = dateProvider.get().minusDays(PR_AGE_LIMIT_IN_DAYS + 10);
         PullRequest oldPullRequest = new PullRequest(PULL_REQUEST_NUMBER);
         oldPullRequest.setCreationDate(oldDate);
 
@@ -78,13 +77,13 @@ public class PullRequestCleaningHandlerTest {
     @Test
     public void shouldNotClosePRWhichAgeIsBelowTheLimit() {
 
-        LocalDateTime recentDate = dateProvider.now().minusDays(1);
+        LocalDateTime recentDate = dateProvider.get().minusDays(1);
         PullRequest recentPullRequest = new PullRequest(PULL_REQUEST_NUMBER);
         recentPullRequest.setCreationDate(recentDate);
 
         pullRequestCleaningHandler.handle(pushEvent, Collections.singletonList(recentPullRequest));
 
-        verifyNoInteractions(remoteSourceControl);
+        verifyZeroInteractions(remoteSourceControl);
 
         assertThat(testAppender.events.stream()
                                       .filter(logEvent -> logEvent.getMDCPropertyMap().getOrDefault("metricName", "NOT_FOUND").equals(OLD_PR_CLOSED))
