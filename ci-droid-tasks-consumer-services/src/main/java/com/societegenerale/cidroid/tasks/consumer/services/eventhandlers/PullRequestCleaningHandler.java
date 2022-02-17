e99fa205444a5bc1518bf72c818d89c91bddfcd5
Vincent Fuchs <vincent.fuchs@gmail.com>
2022-02-17 14:37:27 +0100

    Removing date provider, replaced by standard Supplier<LocalDateTime> (#87)
    Co-authored-by: Vincent Fuchs <vincent.fuchs@gefa.de>

--- ci-droid-tasks-consumer-services/src/main/java/com/societegenerale/cidroid/tasks/consumer/services/eventhandlers/PullRequestCleaningHandler.java
+++ ci-droid-tasks-consumer-services/src/main/java/com/societegenerale/cidroid/tasks/consumer/services/eventhandlers/PullRequestCleaningHandler.java
@@ -1,29 +1,27 @@
 package com.societegenerale.cidroid.tasks.consumer.services.eventhandlers;
 
-import com.societegenerale.cidroid.tasks.consumer.services.RemoteSourceControl;
-import com.societegenerale.cidroid.tasks.consumer.services.model.DateProvider;
-import com.societegenerale.cidroid.tasks.consumer.services.model.SourceControlEvent;
-import com.societegenerale.cidroid.tasks.consumer.services.model.github.PullRequest;
-import com.societegenerale.cidroid.tasks.consumer.services.monitoring.Event;
-import lombok.extern.slf4j.Slf4j;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
 import static com.societegenerale.cidroid.tasks.consumer.services.monitoring.MonitoringAttributes.PR_NUMBER;
 import static com.societegenerale.cidroid.tasks.consumer.services.monitoring.MonitoringAttributes.REPO;
 import static com.societegenerale.cidroid.tasks.consumer.services.monitoring.MonitoringEvents.OLD_PR_CLOSED;
 
+import com.societegenerale.cidroid.tasks.consumer.services.RemoteSourceControl;
+import com.societegenerale.cidroid.tasks.consumer.services.model.SourceControlEvent;
+import com.societegenerale.cidroid.tasks.consumer.services.model.github.PullRequest;
+import com.societegenerale.cidroid.tasks.consumer.services.monitoring.Event;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.function.Supplier;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public class PullRequestCleaningHandler implements PushEventHandler {
 
-    private RemoteSourceControl remoteSourceControl;
-    private DateProvider dateProvider;
-
-    private int prAgeLimitInDays;
+    private final RemoteSourceControl remoteSourceControl;
+    private final Supplier<LocalDateTime>  dateProvider;
+    private final int prAgeLimitInDays;
 
     public PullRequestCleaningHandler(RemoteSourceControl remoteSourceControl,
-                                      DateProvider dateProvider,
+                                      Supplier<LocalDateTime> dateProvider,
                                       int prAgeLimitInDays) {
         this.remoteSourceControl = remoteSourceControl;
         this.dateProvider = dateProvider;
@@ -41,7 +39,7 @@ public class PullRequestCleaningHandler implements PushEventHandler {
 
     private boolean isPullRequestTooOld(PullRequest pullRequest) {
         LocalDateTime creationDate = pullRequest.getCreationDate();
-        LocalDateTime today = dateProvider.now();
+        LocalDateTime today = dateProvider.get();
         return creationDate.plusDays(prAgeLimitInDays).isBefore(today);
     }
 
