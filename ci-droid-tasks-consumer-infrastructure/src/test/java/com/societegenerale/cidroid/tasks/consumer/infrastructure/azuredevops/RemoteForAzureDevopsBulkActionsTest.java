04aa5b716163e6801a8087204f8200a1e32fdea8
Vincent Fuchs <vincent.fuchs@gefa.de>
2022-03-28 18:23:32 +0200

    different port for test

--- ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/azuredevops/RemoteForAzureDevopsBulkActionsTest.java
+++ ci-droid-tasks-consumer-infrastructure/src/test/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/azuredevops/RemoteForAzureDevopsBulkActionsTest.java
@@ -9,26 +9,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.societegenerale.cidroid.tasks.consumer.services.model.github.Repository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 class RemoteForAzureDevopsBulkActionsTest {
 
-  private final static int AZUREDEVOPS_SERVER_PORT_FOR_TESTS=9901;
+  private final static int AZUREDEVOPS_SERVER_PORT_FOR_TESTS=9902;
 
   private final WireMockServer wm = new WireMockServer(
       options()
           .port(AZUREDEVOPS_SERVER_PORT_FOR_TESTS)
           .usingFilesUnderDirectory("src/test/resources/azureDevops"));
 
-  private RemoteForAzureDevopsBulkActions remote=new RemoteForAzureDevopsBulkActions("http://localhost:9901","someApiKey","platform#platform-projects");
+  private final RemoteForAzureDevopsBulkActions remote=new RemoteForAzureDevopsBulkActions("http://localhost:"+AZUREDEVOPS_SERVER_PORT_FOR_TESTS,
+      "someApiKey","platform#platform-projects");
 
   @BeforeEach
   void setup(){
     wm.start();
     configureFor(AZUREDEVOPS_SERVER_PORT_FOR_TESTS);
+  }
+
+
+  @AfterEach
+  void cleanup(){
+    wm.stop();
   }
 
 
@@ -43,7 +50,7 @@ class RemoteForAzureDevopsBulkActionsTest {
 
     var repo=remote.fetchRepository("helm-chart");
 
-    assertThat(repo.get()).extracting(Repository::getFullName).isEqualTo("helm-chart");
+    assertThat(repo.get().getFullName()).isEqualTo("helm-chart");
 
   }
 
