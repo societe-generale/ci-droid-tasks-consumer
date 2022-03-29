810ed3423b9f21c29daeeaa15228d52c99ff6186
Vincent FUCHS <vincent.fuchs@gmail.com>
2022-03-29 21:41:06 +0200

    archunit not null lists

--- ci-droid-tasks-consumer-infrastructure/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/azuredevops/AzureDevopsCommit.java
+++ ci-droid-tasks-consumer-infrastructure/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/azuredevops/AzureDevopsCommit.java
@@ -1,9 +1,13 @@
 package com.societegenerale.cidroid.tasks.consumer.infrastructure.azuredevops;
 
 import java.util.List;
+
+import javax.annotation.Nonnull;
+
 import lombok.Builder;
 import lombok.Getter;
 
+import static java.util.Collections.emptyList;
 
 @Builder
 @Getter
@@ -12,5 +16,8 @@ class AzureDevopsCommit {
   private final String comment;
   private final List<FileChange> changes;
 
-
+  @Nonnull
+  public List<FileChange> getChanges() {
+    return changes==null? emptyList() : changes;
+  }
 }
--- ci-droid-tasks-consumer-infrastructure/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/azuredevops/SuccessfulPush.java
+++ ci-droid-tasks-consumer-infrastructure/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/azuredevops/SuccessfulPush.java
@@ -1,12 +1,20 @@
 package com.societegenerale.cidroid.tasks.consumer.infrastructure.azuredevops;
 
 import java.util.List;
+
+import javax.annotation.Nonnull;
+
 import lombok.Data;
+
+import static java.util.Collections.emptyList;
 
 @Data
 class SuccessfulPush {
 
   private List<PushedCommit> commits;
 
-
+  @Nonnull
+  public List<PushedCommit> getCommits() {
+    return commits==null ? emptyList() : commits;
+  }
 }
