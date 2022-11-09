3fd4d6908f9209632d5817850a6cfd11ae860401
Vincent FUCHS <vincent.fuchs@gmail.com>
2022-11-09 22:51:32 +0100

    Revert "this is instantiated in BitBucketConfig - it fails if it's also scanned"
    This reverts commit 0dc5c4112b566f347e86158c9c095df0df2f0098.
    
    build is failing so reverting, but issue is valid, and we hade the same with GitHub : application doesn't start if synchronous-mode: true . Will create an issue for it

--- ci-droid-tasks-consumer-bitbucket-adapter/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/bitbucket/rest/BitBucketSourceControlEventController.java
+++ ci-droid-tasks-consumer-bitbucket-adapter/src/main/java/com/societegenerale/cidroid/tasks/consumer/infrastructure/bitbucket/rest/BitBucketSourceControlEventController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 @RequestMapping(value = "/cidroid-sync-webhook")
 @Slf4j
 // Todo validate
